478125a555f6f277ee83f1d9a81c3453a3ffc71c
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-20 23:46:36 +0500

    ask today class or not and friend go to university or not then display he will go

---
+++ task2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    string today;
+    string frnd;
+    cout<<"your class is today or not:";
+    cin>> today;
+    cout<<"your friend going to university or not:";
+    cin>> frnd;
+    if(today == "yes"  )
+    {   
+        if( frnd == "yes")
+        {
+            cout<<"you will go to the university:";
+        }
+        
+    }
+}
