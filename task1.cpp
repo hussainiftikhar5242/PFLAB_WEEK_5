1c568738a7f5f35f89796f7507fcec92a32b062a
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-20 23:44:45 +0500

    ask cost and brand and display it can buy or not.

---
+++ task1.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+main()
+{
+    int price=0;
+    string brand;
+    cout<<"enter a cost:";
+    cin>>price;
+    cout<<"enter brand name:";
+    cin>> brand;
+    if(price<1500)
+    {
+        if(brand=="MTJ")
+        {
+            cout<<"you will buy the dress:";
+        }
+    }
+    else 
+    {
+        cout<<"sorry you cann't buy:";
+    }
+}
