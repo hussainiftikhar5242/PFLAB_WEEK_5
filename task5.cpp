77e98a8a6a3323ce0e991802b1a4d9c81e9ccc5c
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-20 23:54:22 +0500

    ask today class or not and friend go to university or not then display he will go  using and operator

---
+++ task5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    string today;
+    string frnd;
+    cout<<"your class is today or not:";
+    cin>> today;
+    cout<<"your friend going to university or not:";
+    cin>> frnd;
+    if(today == "yes" && frnd == "yes" )
+    {  
+        
+        cout<<"you will go to the university:";
+        
+    }
+}
