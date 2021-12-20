598f73a00259004162983f18276d0ed126ddfe51
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-20 23:55:09 +0500

    ask name and roll number and print congratulate. using and operator in single staement.

---
+++ task6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int roll_no;
+    cout<<"enter student name:";
+    cin>> name;
+    cout<<"enter roll number:";
+    cin>>roll_no;
+    if(name == "ali" && roll_no == 501)
+    {
+        cout<<"congratulate "<<name;
+       
+    
+    }
+}
