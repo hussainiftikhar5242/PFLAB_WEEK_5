88b01279fcb1bb4dbed80c1e91b24bbe3ce67129
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-20 23:51:18 +0500

    ask name and roll number and print congratulate.

---
+++ task3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int roll_no;
+    cout<<"enter student name:";
+    cin>> name;
+    cout<<"enter roll number:";
+    cin>>roll_no;
+    if(name == "ali" )
+    {
+        if(roll_no == 501)
+        {
+             cout<<"congratulate "<<name;
+        }
+       
+    
+    }
+}
